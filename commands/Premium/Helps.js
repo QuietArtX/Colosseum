60a5959c637d0c72cec612c6beb2a337da9fba4f
QuietArtX <nakalkali321@gmail.com>
2023-02-07 05:27:09 +0700

    *Add Premium Help

--- commands/Premium/Helps.js
+++ commands/Premium/Helps.js
@@ -16,109 +16,108 @@ module.exports = {
     inVoice: false,
     sameVoice: false,
     },
-    
+  
   run: async (interaction, client, user) => {
-    await interaction.deferReply({ ephemeral: false });
-    
-    const blink = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-      .setLabel(`BUY`)
-      .setEmoji('917981825736011846')
-      .setStyle(ButtonStyle.Link)
-      .setURL('https://discord.com/users/989430735561715712')
-      );
-    
-    const bhome = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-      .setCustomId("home")
-      .setLabel("🏠")
-      .setStyle(ButtonStyle.Success)
-    );
-    
-    const bp1 = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-      .setCustomId("feature")
-      .setLabel("FEATURES")
-      .setEmoji("1013750658643546122")
-      .setStyle(ButtonStyle.Secondary)
-    );
-    
-    const bp2 = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-      .setCustomId("payment")
-      .setLabel("PAYMENT")
-      .setEmoji("1013750700087451750")
-      .setStyle(ButtonStyle.Secondary)
-    );
-    
-    const bp3 = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-      .setCustomId("price")
-      .setLabel("PRICE")
-      .setEmoji("1013750577089478707")
-      .setStyle(ButtonStyle.Secondary)
-    );
+    await interaction.deferReply({ ephemeral: true });
     
     const embed = new EmbedBuilder()
-    .setTitle(`PREMIUM SYSTEM`)
-    .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
-    .setColor(client.color)
-    .setDescription(`If You Want To Buy Premium Commands, Click the buy button below\nAnd After purchasing you will be able to access Premium Commands!`)
-    
-    const m = await interaction.editReply({ embeds: [embed], components: [blink, bp1, bhome, bp2, bp3] });
-    
-    const collector = m.createMessageComponentCollector({
-      filter: (b) => {
-        if (b.user && b.message.author.id == client.user.id) return true;
-        else {
-          b.reply({ embeds: [new EmbedBuilder().setColor(client.color).setDescription(`You Can't Use This Button!! Lets Make Your Own Button`)], ephemeral: true });
-          return false;
-        };
-      },
-      time: 60000 
-    });
-    
-    collector.on('end', async (collected, reason) => {
-      if(reason === 'time') {
-        const timbed = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Deleted! Message timed out, try again`)
-        m.edit({ embeds: [timbed], components: [] })
-      }
-    });
-    collector.on('collect', async (b) => {
-      if (!b.deffered) await b.deferUpdate()
-      if (b.customId === "home") {
-        if (!m) return;
-        return await m.edit({ embeds: [embed], components: [blink, bp1, bhome, bp2, bp3] })
-      }
-      if (b.customId === "feature") {
-        const embed = new EmbedBuilder()
-        .setColor(client.color)
-        .setTitle(`FEATURES`)
-        .setDescription(`\`\`\`yaml\n\u200b\nAUTOPLAY\nPLAYLIST [\n  ▸ Add\n  ▸ Create\n  ▸ Delete\n  ▸ Detail\n  ▸ Import\n  ▸ Private\n  ▸ Public\n  ▸ Remove\n  ▸ Save Current\n  ▸ Save Queue\n  ▸ View\n          ]\nSETUP\n\`\`\``)
-        return await m.edit({ embeds: [embed], components: [blink, bp1, bhome, bp2, bp3] })
-      }
-      if (b.customId === "payment") {
-        const embed = new EmbedBuilder()
-        .setColor(client.color)
-        .setTitle(`PAYMENT METHOD`)
-        .setDescription(`\`\`\`yaml\n\u200b\n ▸ OwO Cash\n ▸ DANA\n ▸ Shopeepay\n\`\`\``)
-        .setFooter({ text: `If you don't have Indonesian Payments, please pay via OwO Cash only` })
-        return await m.edit({ embeds: [embed], components: [blink, bp1, bhome, bp2, bp3] })
-      }
-      if (b.customId === "price") {
-        const embed = new EmbedBuilder()
-        .setColor(client.color)
-        .setTitle(`PRICE LIST`)
-        .setDescription(`\`\`\`yaml\nOwO Cash:\n ▸ Daily = 300K OwO Cash\n ▸ Weekly = 1M OwO Cash\n ▸ Monthly = 3M OwO Cash\n ▸ Yearly = 10M OwO Cash\n ▸ Lifetime = 25M OwO Cash\n\nDANA:\n ▸ Daily = 5.000 IDR\n ▸ Weekly = 10.000 IDR\n ▸ Monthly = 20.000 IDR\n ▸ Yearly = 50.000 IDR\n ▸ Lifetime = 150.000 IDR\`\`\``)
-        return await m.edit({ embeds: [embed], components: [blink, bp1, bhome, bp2, bp3] })
-      }
-    });
+      .setColor(client.color)
+      .setTitle(`${client.username} PREMIUM SYSTEM`)
+      .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
+      .setDescription(`If You Want To Buy Premium Commands, Click the **BUY** button below\nAnd After purchasing you will be able to access Premium Commands!`)
+      
+      const blink = ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setLabel('BUY')
+          .setEmoji('867721862570311700')
+          .setStyle(ButtonStyle.Link)
+          .setURL('https://discord.com/users/989430735561715712')
+        );
+      
+      const row = ActionRowBuilder()
+      new SelectMenuBuilder()
+        .addComponents(
+          .setCustomId('help-prem')
+          .setPlaceholder('Click Here!')
+          .setMinValues(1)
+          .setMaxValues(1)
+          .addOption(
+            {
+              label: 'Features',
+              emoji: '1013750658643546122',
+              value: 'Features',
+            },
+            {
+              label: 'Price List',
+              emoji: '1013750577089478707',
+              value: 'Price,
+            }
+            {
+              label: 'Payment',
+              emoji: '1013750700087451750',
+              value: 'Payment',
+            },
+          ),
+        );
+        const msg = interaction.editReply({ embeds: [embed], components: [blink, row] });
+        const collector = msg.createMessageComponentCollector({
+          fitler: (i) => {
+            if (i.user && i.message.author.id == client.user.id) return true;
+            else {
+              i.reply{( embeds: [new EmbedBuilder().setColor(client.color).setDescription(`You can't use this menu! let's create your own menu\n Type: /help`)], ephemeral: true )};
+              return false;
+            };
+          },
+          time: 60000
+        });
+        
+        collector.on('end' async (collected, reason) => {
+          if (!reason === 'time') {
+            const timbed = new EmbedBuilder()
+              .setColor(client.color)
+              .setTitle(`DELETED`)
+              .setDescription(`Timeout! Please Try Again!`)
+              msg.edit({ embeds: [timbed], components: [] })
+          }
+        });
+        collected.on('collect' async (s) => {
+          if (!s.deffered) await s.deferUpdate()
+          const options = s.values[0]
+          if (options === 'Features') {
+            const nembed = new EmbedBuilder()
+              .setTitle(`PREMIUM FEATURES`)
+              .setColor(client.color)
+              .setDescription(`\`\`\`yaml\n\u200b\nAUTOPLAY\nPLAYLIST [\n  ▸ Add\n  ▸ Create\n  ▸ Delete\n  ▸ Detail\n  ▸ Import\n  ▸ Private\n  ▸ Public\n  ▸ Remove\n  ▸ Save Current\n  ▸ Save Queue\n  ▸ View\n          ]\nSETUP\n\`\`\``);
+              if (!msg) return;
+              return msg.edit({
+                embeds: [nembed],
+                components[blink, row],
+              });
+          }
+          if (options === 'Price') {
+            const nembed = new EmbedBuilder()
+              .setTitle(`PRICE LIST`)
+              .setColor(client.color)
+              .setDescription(`\`\`\`yaml\nOwO Cash:\n ▸ Daily = 300K OwO Cash\n ▸ Weekly = 1M OwO Cash\n ▸ Monthly = 3M OwO Cash\n ▸ Yearly = 10M OwO Cash\n ▸ Lifetime = 25M OwO Cash\n\nDANA & Shopeepay:\n ▸ Daily = 5.000 IDR\n ▸ Weekly = 10.000 IDR\n ▸ Monthly = 20.000 IDR\n ▸ Yearly = 50.000 IDR\n ▸ Lifetime = 150.000 IDR\`\`\``);
+              if (!msg) return;
+              return msg.edit({
+                embeds: [nembed],
+                components[blink, row],
+              });
+          }
+          if (options === 'Payment') {
+            const nembed = new EmbedBuilder()
+              .setTitle(`PAYMENT`)
+              .setColor(client.color)
+              .setDescription(`\`\`\`yaml\n\u200b\n ▸ OwO Cash\n ▸ DANA\n ▸ Shopeepay\n\`\`\``)
+               .setFooter({ text: `If you don't have Indonesian Payments, please pay via OwO Cash only` });
+              if (!msg) return;
+              return msg.edit({
+                embeds: [nembed],
+                components[blink, row],
+              });
+          }
+        })
   }
 }
