ef3e8eba5750ecb5b7d5486fe2add163afddfab3
QuietArtX <nakalkali321@gmail.com>
2023-02-07 00:34:29 +0700

    Test

--- commands/Premium/Helps.js
+++ commands/Premium/Helps.js
@@ -29,17 +29,47 @@ module.exports = {
       .setURL('https://discord.com/users/989430735561715712')
       );
     
+    const per1 = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+      .setCustomId("home")
+      .setLabel("🏠")
+      .setStyle(ButtonStyle.Success)
+    );
+    
+    const per2 = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+      .setCustomId("feature")
+      .setLabel("FEATURES")
+      .setEmoji("1013750658643546122")
+      .setStyle(ButtonStyle.Secondary)
+    );
+    
+    const per3 = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+      .setCustomId("payment")
+      .setLabel("PAYMENT")
+      .setEmoji("1013750700087451750")
+      .setStyle(ButtonStyle.Secondary)
+    );
+    
+    const per3 = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+      .setCustomId("price")
+      .setLabel("PRICE")
+      .setEmoji("1013750577089478707")
+      .setStyle(ButtonStyle.Secondary)
+    );
+    
     const embed = new EmbedBuilder()
     .setTitle(`PREMIUM SYSTEM`)
     .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 2048 }))
     .setColor(client.color)
     .setDescription(`If You Want To Buy Premium Commands Just Click DM Me!\n And After purchasing you will be able to access Premium Commands!`)
     
-    let per1 = new ButtonBuilder().setCustomId("home").setLabel("🏠").setStyle(ButtonStyle.Success);
-    let per2 = new ButtonBuilder().setCustomId("fitur").setLabel("FEATURES").setEmoji("1013750658643546122").setStyle(ButtonStyle.Success);
-    let per3 = new ButtonBuilder().setCustomId("payment").setLabel("PAYMENT").setEmoji("1013750700087451750").setStyle(ButtonStyle.Success);
-    let per4 = new ButtonBuilder().setCustomId("price").setLabel("PRICE LIST").setEmoji("1013750577089478707").setStyle(ButtonStyle.Success);
-    let editEmbed = new EmbedBuilder();
     const m = await interaction.editReply({ embeds: [embed], components: [row, per1, per2, per3, per4] });
     
     const collector = await msg.createMessageComponentCollector({
@@ -55,7 +85,7 @@ module.exports = {
     
     collector.on('end', async () =>{
       if(!m) return;
-      await m.edit({ components: [new ActionRowBuilder().addComponents(row.setDisabled(true), p1.setDisabled(true), p2.setDisabled(true), p3.setDisabled(true), p4.setDisabled(true))] }).catch(() => {});
+      await m.edit({ components: [new ActionRowBuilder().addComponents(row.setDisabled(true), per1.setDisabled(true), per2.setDisabled(true), per3.setDisabled(true), per4.setDisabled(true))] }).catch(() => {});
     });
     collector.on('collect', async (b) => {
       if (!b.deffered) await b.deferUpdate()
@@ -64,16 +94,25 @@ module.exports = {
         return await m.edit({ embeds: [embed], components: [new ActionRowBuilder().addComponents(row, per1, per2, per3, per4)] })
       }
       if (b.customId === "fitur") {
-        editEmbed.setColor(client.color).setTitle(`FEATURES`).setDescription(`\`\`\`yaml\n\u200b\n  ▸ AUTOPLAY\nPLAYLIST [\n  ▸ Add\n  ▸ Create\n  ▸ Delete\n  ▸ Detail\n  ▸ Import\n  ▸ Private\n  ▸ Public\n ▸ Remove\n  ▸ Save Current\n  ▸ Save Queue\n  ▸ View\n          ]\n▸ SETUP\n\`\`\``)
-        return await m.edit({ embeds: [editEmbed], components: [new ActionRowBuilder().addComponents(row, per1, per2, per3, per4)] })
+        const embed = new EmbedBuilder()
+        .setColor(client.color)
+        .setTitle(`FEATURES`)
+        .setDescription(`\`\`\`yaml\n\u200b\n  ▸ AUTOPLAY\nPLAYLIST [\n  ▸ Add\n  ▸ Create\n  ▸ Delete\n  ▸ Detail\n  ▸ Import\n  ▸ Private\n  ▸ Public\n ▸ Remove\n  ▸ Save Current\n  ▸ Save Queue\n  ▸ View\n          ]\n▸ SETUP\n\`\`\``)
+        return await m.edit({ embeds: [embed], components: [new ActionRowBuilder().addComponents(row, per1, per2, per3, per4)] })
       }
       if (b.customId === "payment") {
-        editEmbed.setColor(client.color).setTitle(`PAYMENT METHOD`).setDescription(`\`\`\`yaml\n\u200b\n  ▸OwO Cash\n ▸ DANA\n ▸ Shopeepay\n\`\`\``)
-        return await m.edit({ embeds: [editEmbed], components: [new ActionRowBuilder().addComponents(row, per1, per2, per3, per4)] })
+        const embed = new EmbedBuilder()
+        .setColor(client.color)
+        .setTitle(`PAYMENT METHOD`)
+        .setDescription(`\`\`\`yaml\n\u200b\n  ▸OwO Cash\n ▸ DANA\n ▸ Shopeepay\n\`\`\``)
+        return await m.edit({ embeds: [embed], components: [new ActionRowBuilder().addComponents(row, per1, per2, per3, per4)] })
       }
       if (b.customId === "price") {
-        editEmbed.setColor(client.color).setTitle(`PRICE LIST`).setDescription(`\`\`\`yaml\n\u200b\n  ▸OwO Cash\n ▸ DANA\n ▸ Shopeepay\n\`\`\``).setFooter({ text: `If you don't have Indonesian Payments, please pay via OwO Cash only` })
-        return await m.edit({ embeds: [editEmbed], components: [new ActionRowBuilder().addComponents(row, per1, per2, per3, per4)] })
+        const embed = new EmbedBuilder()
+        .setColor(client.color)
+        .setTitle(`PRICE LIST`)
+        .setDescription(`\`\`\`yaml\n\u200b\n  ▸OwO Cash\n ▸ DANA\n ▸ Shopeepay\n\`\`\``).setFooter({ text: `If you don't have Indonesian Payments, please pay via OwO Cash only` })
+        return await m.edit({ embeds: [embed], components: [new ActionRowBuilder().addComponents(row, per1, per2, per3, per4)] })
       }
     });
   }
