aadbe6db4f4ed88997cb7f9dc386c3500f6747a2
QuietArtX <nakalkali321@gmail.com>
2023-02-07 21:25:10 +0700

    he

--- commands/Moderation/Ban.js
+++ commands/Moderation/Ban.js
@@ -74,7 +74,7 @@ module.exports = {
       });
       
       collector.on('collect', async (b) =>{
-        if(!b.deffered) await deferUpdate()
+        if(!b.deffered) await b.deferUpdate()
         if(b.user.id !== user.id) return;
         switch(b.customId) {
           case "b-yes":
