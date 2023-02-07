5be4b6c1ab076b84ea53f91733833d84bd0f5945
QuietArtX <nakalkali321@gmail.com>
2023-02-07 12:13:01 +0700

    blob

---
+++ commands/Moderation/Ban.js
@@ -0,0 +1,51 @@
+const {
+  EmbedBuilder,
+  ApplicationCommandOptionType,
+} = require("discord.js");
+
+module.exports = {
+  name: ["ban"],
+  description: "ban user from server",
+  category: "Moderation",
+  options: [
+    {
+      name: "target",
+      description: "mention a user",
+      required: true,
+      type: ApplicationCommandOptionType.User,
+    },
+    {
+      name: "reason",
+      description: "provide a reason",
+    },
+    ],
+    permissions: {
+        channel: [],
+        bot: ["Administration"],
+        user: ["Administrator"]
+    },
+    settings: {
+        isPremium: false,
+        isPlayer: false,
+        isOwner: true,
+        inVoice: false,
+        sameVoice: false,
+    },
+    run: async (interaction, client, user) => {
+      const user = interaction.option.getUser("target");
+      const reason = interaction.option.getString("reason");
+      const member = await interaction.guild.members
+        .fetch(user.id)
+        .catch(console.error);
+      
+      if (!reason) reason = "no reason provided";
+      
+      await member.ban({
+        deleteMessageDays: 1,
+        reason: reason,
+      })
+      await interaction.reply({ 
+        content: `Succesful! ${user.tag} Has Banned From This Server`
+      })
+    }
+}
---
+++ commands/Moderation/Kick.js
@@ -0,0 +1,48 @@
+const {
+  EmbedBuilder,
+  ApplicationCommandOptionType,
+} = require("discord.js");
+
+module.exports = {
+  name: ["kick"],
+  description: "Kick user from server",
+  category: "Moderation",
+  options: [
+    {
+      name: "target",
+      description: "mention a user",
+      required: true,
+      type: ApplicationCommandOptionType.User,
+    },
+    {
+      name: "reason",
+      description: "provide a reason",
+    },
+    ],
+    permissions: {
+        channel: [],
+        bot: ["Administration"],
+        user: ["Administrator"]
+    },
+    settings: {
+        isPremium: false,
+        isPlayer: false,
+        isOwner: true,
+        inVoice: false,
+        sameVoice: false,
+    },
+    run: async (interaction, client, user) => {
+      const user = interaction.option.getUser("target");
+      const reason = interaction.option.getString("reason");
+      const member = await interaction.guild.members
+        .fetch(user.id)
+        .catch(console.error);
+      
+      if (!reason) reason = "no reason provided";
+      
+      await member.kick(reason)
+      await user.send({ 
+        content: ` ${user.tag} Has been kicked From This Server`
+      });
+    }
+}
