034a8dfa02429c825238262adfdcf0d36a319dff
QuietArtX <nakalkali321@gmail.com>
2023-02-07 07:35:06 +0700

    .

--- commands/Premium/List.js
+++ commands/Premium/List.js
@@ -39,7 +39,7 @@ module.exports = {
         )}:F> `;
       });
 
-    interaction.reply({
+    interaction.editReply({
       embeds: [
         new EmbedBuilder()
           .setTitle(`All Premium Users`)
