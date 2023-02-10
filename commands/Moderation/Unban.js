df7330657c6a5203f640f5607b1f05c07c673445
QuietArtX <nakalkali321@gmail.com>
2023-02-10 17:14:11 +0700

    Add Moderation

---
+++ commands/Moderation/Unban.js
@@ -0,0 +1,116 @@
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ApplicationCommandOptionType,
+  ComponentType
+} = require("discord.js");
+
+module.exports = {
+  name: ["ban"],
+  description: "ban member from this server",
+  category: "Moderation",
+  options: [
+    {
+      name: "userid",
+      description: "mention a target for ban",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    }
+  ],
+  permissions: {
+        channel: [],
+        bot: ["BanMembers"],
+        user: ["BanMembers"]
+    },
+  settings: {
+        isPremium: false,
+        isPlayer: false,
+        isOwner: false,
+        inVoice: false,
+        sameVoice: false,
+  },
+  
+  run: async (interaction, client, user) => {
+    await interaction.deferReply({ ephemeral: false });
+    
+    const { channel, options } = interaction;
+    
+    const userId = options.getUser("userid");
+    const member = await interaction.guild.members.unban(user.id)
+    
+    const msg = await interaction.editReply({
+      embeds: [
+        new EmbedBuilder()
+        .setColor(client.color)
+        .setTitle(`UNBAN PENDING!`)
+        .setDescription(`ARE YOU SURE FOR UNBAN THIS MEMBER?\n－－－－－－－\n◈ User: <@${member}>\n－－－－－－－`)
+        .setFooter({
+          text: `Colosseum Music Moderator`
+        })
+        .setTimestamp()
+      ],
+      components: [
+        new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+          .setCustomId(`yes`)
+          .setLabel('YES')
+          .setStyle(ButtonStyle.Danger),
+          new ButtonBuilder()
+          .setCustomId(`no`)
+          .setLabel('NO')
+          .setStyle(ButtonStyle.Secondary)
+        )
+      ]
+    });
+    
+    const collector = msg.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time: 25000
+    });
+    
+    collector.on('collect', async (b) => {
+      if (b.customId === "yes") {
+        await member.ban({reason})
+        interaction.editReply({
+          embeds: [
+            new EmbedBuilder()
+            .setColor(client.color)
+            .setTitle(`UNBAN SUCCESS`)
+            .setDescription(`SUCCESSFUL UNBANNED!\n－－－－－－－\n◈ User: <@${member}>\n－－－－－－－`)
+            .setFooter({
+              text: `Colosseum Music Moderator`
+            })
+            .setTimestamp()
+          ],
+          components: []
+        });
+      }
+      if (b.customId === "no") {
+        interaction.editReply({
+          embeds: [
+            new EmbedBuilder()
+            .setColor(client.color)
+            .setTitle(`UNBAN CANCEL`)
+            .setDescription(`BANNED CANCELED`)
+            .setFooter({
+              text: `Colosseum Music Moderator`
+            })
+            .setTimestamp()
+          ],
+          components: []
+        });
+      }
+    });
+    
+    collector.on('end', async (collected, reason) => {
+      if (reason === "time") {
+        const timbed = new EmbedBuilder()
+        .setColor(client.color)
+        .setDescription(`Timeout! Please Try Again!`)
+        msg.edit({ embeds: [timbed], components: [] }).then (msg => msg.delete({ timeout: 6000 }))
+      }
+    });
+  }
+}
