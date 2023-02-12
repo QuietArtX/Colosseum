7412e5972e29b8a519a36941dc66b351bd14f445
QuietArtX <nakalkali321@gmail.com>
2023-02-12 21:32:28 +0700

    .

--- commands/Utility/Clone.js
+++ commands/Utility/Clone.js
@@ -11,7 +11,6 @@ module.exports = {
   name: ["bot", "clone"],
   description: "Clone bot for your discord server",
   category: "Utility",
-  ooption: [],
   permissions: {
         channel: [],
         bot: [],
