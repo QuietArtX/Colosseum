2ee91cddf3ce9ca85271ae90a5404de5873dec2b
QuietArtX <nakalkali321@gmail.com>
2023-02-12 21:16:19 +0700

    .

---
+++ commands/Utility/Clone.js
@@ -0,0 +1,56 @@
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ApplicationCommandOptionType,
+  ComponentType
+} = require("discord.js");
+
+module.exports = {
+  name: ["invite"],
+  description: "Invite me to your seever",
+  category: "Utility",
+  ooption: [],
+  permissions: {
+        channel: [],
+        bot: [],
+        user: []
+    },
+  settings: {
+        isPremium: true,
+        isPlayer: false,
+        isOwner: false,
+        inVoice: false,
+        sameVoice: false,
+  },
+  
+  run: async (interaction, client, user) => {
+    await interaction.deferReply({ ephemeral: false });
+    
+    const cEmbed = new EmbedBuilder()
+    .setTitle(`!! CLONE WARNING !!`)
+    .setDescription(`U Want To Clone This Bot?\nwhere can i get this code? Click The Button Below\n\n**JUST PUT YOUR BOT TOKEN IN ENV SECRET & DON'T CHANGE ANYTHING**`)
+    .setColor(client.color)
+    .setTimestamp();
+    
+    const cButton = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+      .setLabel('Repl.it')
+      .setEmoji('1045961018645422080')
+      .setStyle(ButtonStyle.Link)
+      .setURL('https://replit.com/github/QuietArtX/Colloseum'),
+      new ButtonBuilder()
+      .setLabel('Fork on GitHub')
+      .setEmoji('1064244587541045319')
+      .setStyle(ButtonBuilder.Link)
+      .setURL('https://github.com/QuietArtX/Colloseum/fork'
+    );
+    
+    await interaction.editReply({
+      embeds: [cEmbed],
+      components: [cButton],
+      ephemeral: true
+    });
+  }
+}
